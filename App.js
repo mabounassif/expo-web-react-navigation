ceff1197ac6a45ab20328a349b3da05dc78a5e2a
mabounassif <mahmoud.abounassif@gmail.com>
2023-07-12 10:15:49 -0400

    Example of unexpected navigation

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Text } from 'react-native';
+// import * as Linking from "expo-linking";
+
+const linking = {
+  prefixes: [],
+  enabled: true,
+  screens: {
+    initialRouteName: 'Home',
+    Home: '',
+    Privacy: 'privacy',
+  },
+};
+
+const RootStack = createNativeStackNavigator();
 
 export default function App() {
+  const header = () => <Text>Header</Text>;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer linking={linking} fallback={<Text>Loading...</Text>}>
+      <RootStack.Navigator>
+        <RootStack.Screen name="Home" component={() => <Text>Hello</Text>} />
+        <RootStack.Screen name="Privacy" component={() => <Text>But</Text>} />
+      </RootStack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
